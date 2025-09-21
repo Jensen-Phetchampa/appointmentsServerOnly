3159a8d17c6d7eb4896df7cf08edff9abfbd56b5
Jensen Phetchampa <phetchampaj1@southernct.edu>
2025-09-21 17:19:46 +0000

    Completing HW 2

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ let serverObj =  http.createServer(function(req,res){
 		case "/cancel":
 			cancel(urlObj.query,res);
 			break;
+		case "/check":
+			check(urlObj.query,res);
+			break;
 		default:
 			error(res,404,"pathname unknown");
 
@@ -29,10 +32,18 @@ let serverObj =  http.createServer(function(req,res){
 });
 
 function schedule(qObj,res) {
-	if (availableTimes[qObj.day].some(time => time == qObj.time))
+	if (!qObj.name || !qObj.day || !qObj.time) {
+        	return error(res, 400, "Missing query parameters");
+	}
+
+	if (availableTimes[qObj.day] && availableTimes[qObj.day].some(time => time == qObj.time))
 	{
+		availableTimes[qObj.day] = availableTimes[qObj.day].filter(t => t !== qObj.time);
+
+		appointments.push({ name: qObj.name, day: qObj.day, time: qObj.time });
+
 		res.writeHead(200,{'content-type':'text/plain'});
-		res.write("scheduled");
+		res.write("Appointment scheduled");
 		res.end();
 	}
 	else 
@@ -41,7 +52,47 @@ function schedule(qObj,res) {
  
 }
 
-function cancel(qObj) {
+function cancel(qObj, res) {
+	if (!qObj.name || !qObj.day || !qObj.time) {
+        return error(res, 400, "Missing query parameters");
+    }
+	let index = appointments.findIndex(appt =>
+        appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time
+    );
+
+    if (index !== -1) {
+        
+        appointments.splice(index, 1);
+
+        
+        if (availableTimes[qObj.day]) {
+            availableTimes[qObj.day].push(qObj.time);
+        } else {
+            availableTimes[qObj.day] = [qObj.time];
+        }
+
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write("Appointment has been canceled");
+        res.end();
+    } else {
+        error(res, 404, "Appointment not found");
+    }
+}
+
+function check(qObj, res) {
+    if (!qObj.day || !qObj.time) {
+        return error(res, 400, "Missing query parameters");
+    }
+
+    if (availableTimes[qObj.day] && availableTimes[qObj.day].some(time => time == qObj.time)) {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write("Time is available");
+        res.end();
+    } else {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write("Time is NOT available");
+        res.end();
+    }
 }
 
 function error(response,status,message) {
