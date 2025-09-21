e0582592932b48b3cac2220e7a8e19d13a543c02
Jensen Phetchampa <phetchampaj1@southernct.edu>
2025-09-21 17:09:20 +0000

    .

--- app.js
+++ app.js
@@ -8,101 +8,47 @@ const availableTimes = {
     Thursday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
     Friday: ["1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
 };
-
 const appointments = [
-    { name: "James", day: "Wednesday", time: "3:30" },
-    { name: "Lillie", day: "Friday", time: "1:00" }
-];
+    {name: "James", day: "Wednesday", time: "3:30" },
+    {name: "Lillie", day: "Friday", time: "1:00" }];
 
-let serverObj = http.createServer(function (req, res) {
-    console.log(req.url);
-    let urlObj = url.parse(req.url, true);
+let serverObj =  http.createServer(function(req,res){
+	console.log(req.url);
+	let urlObj = url.parse(req.url,true);
+	switch (urlObj.pathname) {
+		case "/schedule":
+			schedule(urlObj.query,res);
+			break;
+		case "/cancel":
+			cancel(urlObj.query,res);
+			break;
+		default:
+			error(res,404,"pathname unknown");
 
-    switch (urlObj.pathname) {
-        case "/schedule":
-            schedule(urlObj.query, res);
-            break;
-        case "/cancel":
-            cancel(urlObj.query, res);
-            break;
-        case "/check":
-            check(urlObj.query, res);
-            break;
-        default:
-            error(res, 404, "pathname unknown");
-    }
+	}
 });
 
-function schedule(qObj, res) {
-    if (!qObj.name || !qObj.day || !qObj.time) {
-        return error(res, 400, "Missing query parameters");
-    }
+function schedule(qObj,res) {
+	if (availableTimes[qObj.day].some(time => time == qObj.time))
+	{
+		res.writeHead(200,{'content-type':'text/plain'});
+		res.write("scheduled");
+		res.end();
+	}
+	else 
+		error(res,400,"Can't schedule");
 
-    if (availableTimes[qObj.day] && availableTimes[qObj.day].some(time => time == qObj.time)) {
-        // Reserve: remove from availableTimes
-        availableTimes[qObj.day] = availableTimes[qObj.day].filter(t => t !== qObj.time);
-
-        // Add to appointments
-        appointments.push({ name: qObj.name, day: qObj.day, time: qObj.time });
-
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write("Appointment scheduled");
-        res.end();
-    } else {
-        error(res, 400, "Can't schedule: time not available");
-    }
+ 
 }
 
-function cancel(qObj, res) {
-    if (!qObj.name || !qObj.day || !qObj.time) {
-        return error(res, 400, "Missing query parameters");
-    }
-
-    // Find the appointment
-    let index = appointments.findIndex(appt =>
-        appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time
-    );
-
-    if (index !== -1) {
-        // Remove from appointments
-        appointments.splice(index, 1);
-
-        // Return the time slot to availableTimes
-        if (availableTimes[qObj.day]) {
-            availableTimes[qObj.day].push(qObj.time);
-        } else {
-            availableTimes[qObj.day] = [qObj.time];
-        }
-
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write("Appointment has been canceled");
-        res.end();
-    } else {
-        error(res, 404, "Appointment not found");
-    }
+function cancel(qObj) {
 }
 
-function check(qObj, res) {
-    if (!qObj.day || !qObj.time) {
-        return error(res, 400, "Missing query parameters");
-    }
+function error(response,status,message) {
 
-    if (availableTimes[qObj.day] && availableTimes[qObj.day].some(time => time == qObj.time)) {
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write("Time is available");
-        res.end();
-    } else {
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write("Time is NOT available");
-        res.end();
-    }
+	response.writeHead(status,{'content-type':'text/plain'});
+	response.write(message);
+	response.end();
 }
 
-function error(response, status, message) {
-    response.writeHead(status, { 'content-type': 'text/plain' });
-    response.write(message);
-    response.end();
-}
-
-serverObj.listen(80, function () { console.log("listening on port 80") });
-
+serverObj.listen(80,function(){console.log("listening on port 80")});
